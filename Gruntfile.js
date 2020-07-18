4cd1719f0ff62ddedab4d46cef7273e8ec5a64b5
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2020-07-18 11:54:15 +0800

    自动化

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
       files: {
         expand: true,
         cwd: '_book',
-        src: ['*.html', 'chapters/*.html'],
+        src: ['*.html', 'chapters/**/*.html'],
         dest: 'dist/'
       }
     },
